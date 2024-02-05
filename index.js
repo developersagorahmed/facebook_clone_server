13d75907bbf3fb40a123a96dc9a0f3b2691ecac7
Sagor Ahmed <developersagorahmed@gmail.com>
2024-02-05 15:57:38 +0600

    second commit

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7is7xhq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://facebook2024:iSTIxjwbA0JymQx1@cluster0.7is7xhq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
 	serverApi: {
 		version: ServerApiVersion.v1,
+
 		strict: true,
 		deprecationErrors: true,
 	},
